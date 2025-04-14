4ade00e40e117ba0c50b7aacbce8fb04eb667623
Juan Pablo Figueroa <juan-pablo@Juans-MacBook-Air.local>
2025-04-15 00:09:11 +0300

    add player groups

--- main.py
+++ main.py
@@ -8,21 +8,36 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
-    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    dt = 0
+
     while True:
-        screen.fill("black")
-        player.draw(screen)
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        #player.draw(screen)
+        pygame.display.flip()
+
         delta = clock.tick(60)
         dt = delta/1000
-        player.update(dt)
+        # player.update(dt)
+
+
 
 if __name__ == "__main__":
     main()

868d274f98b7975f285f55e09a3a08ed51353364
Juan Pablo Figueroa <juan-pablo@Juans-MacBook-Air.local>
2025-04-15 23:46:30 +0300

    add asteroid splitting logic

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,6 @@
 from circleshape import *
 from constants import *
+import random
 
 class Asteroid(CircleShape):
     """
@@ -10,6 +11,24 @@ class Asteroid(CircleShape):
     def draw(self, screen):
         color = "red"
         pygame.draw.circle(screen,color,self.position, self.radius,width=2)
+
+    def split(self):
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            self.kill()
+            return
+        else:
+            random_angle = random.uniform(20,50)
+            # Use the rotate method on the asteroid's velocity vector to create 2 new vectors,
+            # that are rotated by random_angle and -random_angle respectively
+            # (they should split in opposite directions).
+            vector_1 = self.velocity.rotate(random_angle)
+            vector_2 = self.velocity.rotate(-random_angle)
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            new_asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+            new_asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+            new_asteroid_1.velocity = vector_1*1.2
+            new_asteroid_2.velocity = vector_2*1.2
+            self.kill()
 
     def update(self, dt):
         """
--- main.py
+++ main.py
@@ -44,6 +44,10 @@ def main():
             if asteroid.check_collision(player):
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if asteroid.check_collision(shot):
+                    asteroid.split()
+                    shot.kill()
 
         screen.fill("black")
 
